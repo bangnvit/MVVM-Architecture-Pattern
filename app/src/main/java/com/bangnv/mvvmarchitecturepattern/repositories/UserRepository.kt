16aabba8ebef67bef9b52df484cd30ca6bf7cb5a
BangNV <bangnv.it@gmail.com>
2024-10-16 11:59:24 +0700

    fix default example

--- app/src/main/java/com/bangnv/mvvmarchitecturepattern/repositories/UserRepository.kt
+++ app/src/main/java/com/bangnv/mvvmarchitecturepattern/repositories/UserRepository.kt
@@ -6,6 +6,6 @@ import com.bangnv.mvvmarchitecturepattern.models.User
 class UserRepository {
     fun getUserData(): User {
         // Fetch weather data from a remote server or local storage
-        return User("Nam", "nam123@gmail.com")
+        return User("Bang", "Bang123@xmail.com")
     }
 }
