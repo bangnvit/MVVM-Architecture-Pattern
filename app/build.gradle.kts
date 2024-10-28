724c5fa08f83bd2641f49f7a366dce9132e154e1
BangNV <bangnv.it@gmail.com>
2024-10-28 16:49:00 +0700

    update lib versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,14 +48,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 
     implementation(libs.retrofit)
     implementation(libs.moshi.converter)
